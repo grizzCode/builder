01ddeb44caae11e1be3396d9b8f4c9677d51efde
Charles Buxton <chazbux@gmail.com>
2020-07-16 12:25:34 -0600

    material ui

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <div class="black-line">
-
-    </div>
-    <p>some text</p>
-    <nav class="nav-bar">
-      
-    </nav>
+     
     </>
   );
 }
