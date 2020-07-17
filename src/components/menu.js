5aaa2e8fd471cd00d2a59985ce4189c0c8881791
Charles Buxton <chazbux@gmail.com>
2020-07-17 11:42:19 -0600

    Styling / Dark Theme Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import styled from "styled-components";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
-export default function SimpleMenu() {
+export default function FadeMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -14,22 +18,72 @@ export default function SimpleMenu() {
     setAnchorEl(null);
   };
 
+  const themeObject = {
+    palette: {
+      type: 'dark'
+    }
+  }
+
+  const themeConfig = createMuiTheme(themeObject)
+
   return (
-    <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Open Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
+    <NavDiv>
+      <ThemeProvider theme={themeConfig}>
+        <StyledButton aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+          Projects ^
+      </StyledButton>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>Project 1</MenuItem>
+          <MenuItem onClick={handleClose}>Project 2</MenuItem>
+          <MenuItem onClick={handleClose}>Project 3</MenuItem>
+          <MenuItem onClick={handleClose}>Project 4</MenuItem>
+          <MenuItem onClick={handleClose}>Project 5</MenuItem>
+          <MenuItem onClick={handleClose}>Project 6</MenuItem>
+        </Menu>
+        <StyledButton aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+          Process ^
+      </StyledButton>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>Project 1</MenuItem>
+          <MenuItem onClick={handleClose}>Project 2</MenuItem>
+          <MenuItem onClick={handleClose}>Project 3</MenuItem>
+          <MenuItem onClick={handleClose}>Project 4</MenuItem>
+          <MenuItem onClick={handleClose}>Project 5</MenuItem>
+          <MenuItem onClick={handleClose}>Project 6</MenuItem>
+        </Menu>
+      </ThemeProvider>
+    </NavDiv>
   );
 }
+
+const StyledButton = styled(Button)`
+  &&& {
+    font-family: 'Work Sans', sans-serif;
+    color: white;
+    margin-right: 20px;
+  }
+`;
+
+const NavDiv = styled.div`
+  width: 75%;
+  padding: 8px;
+  background-color: black;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+
+`
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from './menu'
+
+export default function Navbar() {
+
+
+
+  return (
+    <div>
+      <Menu/>
+    </div>
+  )
+}
