89202988106d5cc053bd5039c657ed67d05939ae
Charles Buxton <chazbux@gmail.com>
2020-07-24 16:30:23 -0600

    build fix 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import Menu from './Menu'  
-import styled from 'styled-components'
-import Logo from '../images/logo.png'
-import FacebookIcon from '@material-ui/icons/Facebook'
-
 import Slider from './Slideshow'
 
 export default function Home() {
