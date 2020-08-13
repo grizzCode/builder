768f98d5b84d494e5b70e2c9bd90668f4e996462
Charles Buxton <chazbux@gmail.com>
2020-08-13 17:57:29 -0600

    iframe title error fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export default function About() {
           </AddressDiv>
           <AddressDiv>
             <AddressLabel>Office</AddressLabel>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.118626322351!2d-111.46783198459562!3d40.69338497933395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875212d0501c7f27%3A0xfe41af1d32eb6996!2s4376%20N%20Forestdale%20Dr%20%2312%2C%20Park%20City%2C%20UT%2084098!5e0!3m2!1sen!2sus!4v1597360190248!5m2!1sen!2sus" width="600" height="400" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.118626322351!2d-111.46783198459562!3d40.69338497933395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875212d0501c7f27%3A0xfe41af1d32eb6996!2s4376%20N%20Forestdale%20Dr%20%2312%2C%20Park%20City%2C%20UT%2084098!5e0!3m2!1sen!2sus!4v1597360190248!5m2!1sen!2sus" width="600" height="400" frameborder="0" title="office" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
           </AddressDiv>
         </DivCenter>
       </StyledDiv>
