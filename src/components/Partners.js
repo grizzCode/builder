5ba0c57eba996523d56531f0c300e00c0f1e398f
Charles Buxton <cbuxton@Promontory.PromontoryClub.com>
2020-12-30 12:18:37 -0700

    partners images

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Klima from '../images/Klim.JPG'
+import Drift from '../images/Drift Design.JPG'
+import Imbue from '../images/Imbue Design.JPG'
+import Branch from '../images/Branch Design.JPG'
+import Jsquared from '../images/J-squared.JPG'
+import HomeStyle from '../images/Home Style PC.JPG'
 
 export default function Associates() {
 
@@ -7,13 +13,19 @@ export default function Associates() {
     <>
       <StyledText>
         <StyledH4>ARCHITECTS</StyledH4>
-         <p>Chris  : Klima Architecture</p>
-         <p>Andrew: Drift design</p>
-         <p>Imbue design</p>
-         <p>Brach design</p>
+         <p>Klima Architecture</p>
+         <StyledImg src={Klima}></StyledImg>
+         <p>Drift Design</p>
+         <StyledImg src={Drift}></StyledImg>
+         <p>Imbue Design</p>
+         <StyledImg src={Imbue}></StyledImg>
+         <p>Brach Design</p>
+         <StyledImg src={Branch}></StyledImg>
         <StyledH4>DESIGNERS</StyledH4>
-        <p>Julie Chahine : J squared interiors</p>
-        <p>Mette Axboe : Home style Park City</p>
+        <p>J Squared Interiors</p>
+        <StyledImg src={Jsquared}></StyledImg>
+        <p>Home Style Park City</p>
+        <StyledImg src={HomeStyle}></StyledImg>
       </StyledText>
     </>
   )
@@ -33,4 +45,11 @@ const StyledH4 = styled.h3`
   text-transform: uppercase;
   font-weight: 400;
   margin: 20px;
+`
+
+const StyledImg = styled.img`
+width: 120px;
+height: 120px;
+overflow: hidden;
+
 `
