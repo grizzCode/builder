30599e741a1c4a9548cad3e3035d76fcf09baf34
Charles Buxton <cbuxton@Promontory.PromontoryClub.com>
2020-12-30 13:37:37 -0700

    partners desktop complete

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -12,20 +12,66 @@ export default function Associates() {
   return (
     <>
       <StyledText>
-        <StyledH4>ARCHITECTS</StyledH4>
-         <p>Klima Architecture</p>
-         <StyledImg src={Klima}></StyledImg>
-         <p>Drift Design</p>
-         <StyledImg src={Drift}></StyledImg>
-         <p>Imbue Design</p>
-         <StyledImg src={Imbue}></StyledImg>
-         <p>Brach Design</p>
-         <StyledImg src={Branch}></StyledImg>
-        <StyledH4>DESIGNERS</StyledH4>
-        <p>J Squared Interiors</p>
-        <StyledImg src={Jsquared}></StyledImg>
-        <p>Home Style Park City</p>
-        <StyledImg src={HomeStyle}></StyledImg>
+        <Border>
+          <StyledH4>ARCHITECTS</StyledH4>
+        </Border>
+        <ContainerDiv>
+          <StyledA href="http://klimaarchitecture.com/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>Klima Architecture</StyledP>
+                <StyledImg src={Klima}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+          <StyledA href="https://www.driftdesignstudio.co/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>Drift Design</StyledP>
+                <StyledImg src={Drift}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+        </ContainerDiv>
+        <ContainerDiv>
+          <StyledA href="https://imbue.design/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>Imbue Design</StyledP>
+                <StyledImg src={Imbue}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+          <StyledA href="https://www.brachdesign.com/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>Brach Design</StyledP>
+                <StyledImg src={Branch}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+        </ContainerDiv>
+        <Border>
+          <StyledSecondaryH4>DESIGNERS</StyledSecondaryH4>
+        </Border>
+        <ContainerDiv>
+          <StyledA href="https://www.brachdesign.com/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>J Squared Interiors</StyledP>
+                <StyledImg src={Jsquared}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+          <StyledA href="https://www.brachdesign.com/">
+            <AnimationContainer>
+              <FlexDiv>
+                <StyledP>Home Style Park City</StyledP>
+                <StyledImg src={HomeStyle}></StyledImg>
+              </FlexDiv>
+            </AnimationContainer>
+          </StyledA>
+        </ContainerDiv>
       </StyledText>
     </>
   )
@@ -45,11 +91,65 @@ const StyledH4 = styled.h3`
   text-transform: uppercase;
   font-weight: 400;
   margin: 20px;
+  width: 100%;
+  text-align: center;
+`
+const StyledSecondaryH4 = styled.h3`
+  font-family: inherit;
+  font-size: 26px;
+  text-transform: uppercase;
+  font-weight: 400;
+  margin: 20px;
+  width: 100%;
+  text-align: center;
+  margin-top: 6%;
 `
 
 const StyledImg = styled.img`
-width: 120px;
-height: 120px;
-overflow: hidden;
+  width: 220px;
+  height: 220px;
+  overflow: hidden;
+`
+
+const StyledP = styled.p`
+  width: 220px;
+`
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin: auto;
+  margin-top: 4%;
+  text-align: center;
+`
+
+const AnimationContainer = styled.div`
+transition: 0.3s ease-out;
+text-decoration: none;
+margin-top: 6%;
+
+
+&:hover {
+  transform: scale(1.2);
+  transition: 0.3s ease-out;
+  cursor: pointer;
+};
+`
+
+const StyledA = styled.a`
+  text-decoration: none;
+  color: inherit;
+`
+
+const ContainerDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 6%;
+`
+const Border = styled.div`
+  border-bottom: 1px solid lightgrey;
+  width: 70%;
+  margin: auto;
 
 `
